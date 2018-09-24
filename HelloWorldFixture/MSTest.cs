3a84898e8f8ec8d02d90f2ec1f2dfb91d98ea1a6
Daniel Simpson <daniel_simpson1988@hotmail.com>
2018-09-24 11:29:58 +1000

    Add a really minor MSTest

---
+++ HelloWorldFixture/MSTest.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace HelloWorldFixture
+{
+    [TestClass]
+    public class MSTest
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            Console.WriteLine("This is an incredibly useless MS test, except to make sure a test can run.");
+        }
+    }
+}
---
+++ HelloWorldFixture/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("HelloWorldFixture")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("HelloWorldFixture")]
+[assembly: AssemblyCopyright("Copyright ©  2018")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("ed073cf1-ce90-4dc7-8742-024bae512008")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
